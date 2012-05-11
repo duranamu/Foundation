f9a4233fe1b3642b7e771659ef44da95879218be
duranamu <duranamu@126.com>
2012-05-11 15:12:30 +0800

    revolutionary

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,83 @@
+/****************************************************************************
+	Cocos  Project
+			hosting	github.com
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+#include "HelloWorldScene.h"
+#include "Foundation/Foundation.h"
+
+using namespace cocos2d;
+
+CCScene* HelloWorld::scene()
+{
+    CCScene * scene = NULL;
+    do 
+    {
+        // 'scene' is an autorelease object
+        scene = CCScene::node();
+        CC_BREAK_IF(! scene);
+
+        // 'layer' is an autorelease object
+        HelloWorld *layer = HelloWorld::node();
+        CC_BREAK_IF(! layer);
+
+        // add layer as a child to scene
+        scene->addChild(layer);
+    } while (0);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    bool bRet = false;
+    do 
+    {
+
+        CCLabelTTF* pLabel = CCLabelTTF::labelWithString("Hello World", "Thonburi", 64);
+        CC_BREAK_IF(! pLabel);
+
+        // Get window size and place the label upper. 
+        CCSize size = CCDirector::sharedDirector()->getWinSize();
+        pLabel->setPosition(ccp(size.width / 2, size.height - 20));
+
+        // Add the label to HelloWorld layer as a child layer.
+        this->addChild(pLabel, 1);
+
+
+        bRet = true;
+    } while (0);
+
+	NSDate* now = NSDate::distantPast();
+
+	//printf("%ld",now->timesSeconds);
+
+	//
+
+    return bRet;
+}
+
+void HelloWorld::menuCloseCallback(CCObject* pSender)
+{
+    // "close" menu item clicked
+    CCDirector::sharedDirector()->end();
+}
+
